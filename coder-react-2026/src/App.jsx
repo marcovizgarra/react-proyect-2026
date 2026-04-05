002cb517f41eb6bc1c941b66968bc2d32a5a6c4a
MarcoVizgarra_evo980 <marcovizgarra@gmail.com>
2026-04-05 17:29:09 -0300

    Tailwind tests

--- coder-react-2026/src/App.jsx
+++ coder-react-2026/src/App.jsx
@@ -1,17 +1,20 @@
-import './App.css';
 import ComponenteClase3 from './components/clase3/ComponenteClase3.jsx';
 import ComponentePresentacion from './components/clase4/ComponentePresentacion.jsx';
 import Props from './components/clase4/Props.jsx';
 import States from './components/clase4/States.jsx';
+import TailwindTest from './components/tailwindTest/TailwindTest.jsx';
 
 function App() {
   return (
     <>
-      <h1>Título</h1>
       {/* <ComponenteClase3/> */}
       {/* <Props titulo={"Título prop"} subtitulo={"Subtitulo"} /> */}
       {/* <States/> */}
-      <ComponentePresentacion titulo1={"Coder House"} titulo2={"Curso de React JS"}/>
+      {/* <ComponentePresentacion titulo1={"Coder House"} titulo2={"Curso de React JS"}/> */}
+      
+      {/* Hasta la CLASE 4 inclusive utilicé bootstrap */}
+      {/* Desde la CLASE 5 en adelante Tailwind para realizar pruebas */}
+      {/* <TailwindTest /> */}
     </>
   )
 }
